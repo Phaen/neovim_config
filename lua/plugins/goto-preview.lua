398c08d19d992a339058fc79d31bb42c85399edf
Pablo Kebees <pablo@i-aspect.com>
2025-01-24 18:40:19 +0100

    make preview keys match regular ones

--- lua/plugins/goto-preview.lua
+++ lua/plugins/goto-preview.lua
@@ -2,9 +2,18 @@ return {
   "rmagatti/goto-preview",
   event = "BufEnter",
   opts = {
-    default_mappings = true,
+    default_mappings = false,
   },
-  keys = {
-    { "gp", "", desc = "Preview", mode = "n" },
-  },
+  keys = function()
+    return {
+      { "gp", "", desc = "Preview", mode = "n" },
+      { "gpd", require("goto-preview").goto_preview_definition, desc = "Preview definition" },
+      { "gpy", require("goto-preview").goto_preview_type_definition, desc = "Preview type definition" },
+      { "gpI", require("goto-preview").goto_preview_implementation, desc = "Preview implementation" },
+      { "gpD", require("goto-preview").goto_preview_declaration, desc = "Preview declaration" },
+      { "gP", require("goto-preview").close_all_win, desc = "Close preview windows" },
+      -- Requires Telescope
+      --{ "gpr",  require("goto-preview").goto_preview_references() , desc = "Preview references" },
+    }
+  end,
 }
